e56aed00b18769d5d0c384386e40e8fdb9868b0d
bsahd <193844131+bsahd@users.noreply.github.com>
2025-05-09 18:11:26 +0900

    simpler sql generator

---
+++ sql.js
@@ -0,0 +1,105 @@
+
+class Condition {
+	toString() {
+		throw new Error("実装してね");
+	}
+}
+
+class BinaryCondition extends Condition {
+	constructor(col, op, val) {
+		super();
+		this.col = col;
+		this.op = op;
+		this.val = val;
+	}
+	toString() {
+		const v = typeof this.val === "string" ? `"${this.val}"` : this.val;
+		return `${this.col} ${this.op} ${v}`;
+	}
+}
+
+class LogicCondition extends Condition {
+	constructor(op, conds) {
+		super();
+		this.op = op;
+		this.conds = conds;
+	}
+	toString() {
+		return this.conds.map((c) => `(${c.toString()})`).join(` ${this.op} `);
+	}
+}
+
+class Query {
+	constructor(table) {
+		this.table = table;
+		this.whereCond = null;
+		this.orders = [];
+	}
+	where(cond) {
+		if (this.whereCond) {
+			throw new Error("cant define where conditions twice");
+		}
+		this.whereCond = cond;
+		return this;
+	}
+	order(col, desc) {
+		if (typeof col == "undefined" || typeof desc == "undefined") {
+			throw new Error("please specify col and asc/desc");
+		}
+		this.orders.push({
+			col,
+			desc,
+		});
+		return this;
+	}
+	toString() {
+		let sql = `SELECT * FROM ${this.table}`;
+		if (this.whereCond) sql += ` WHERE ${this.whereCond.toString()}`;
+		if (this.orders.length > 0)
+			sql += ` ORDER BY ${this.orders
+				.map((a) => `${a.col} ${a.desc ? "DESC" : "ASC"}`)
+				.join(", ")}`;
+		return sql;
+	}
+}
+
+const SQL = {
+	builder: {
+		table(name) {
+			return new Query(name);
+		},
+	},
+	ASC: false,
+	DESC: true,
+	eq(col, val) {
+		return new BinaryCondition(col, "=", val);
+	},
+	gte(col, val) {
+		return new BinaryCondition(col, ">=", val);
+	},
+	lte(col, val) {
+		return new BinaryCondition(col, "<=", val);
+	},
+	including(col, v) {
+		return new BinaryCondition(col, "LIKE", `%${v}%`);
+	},
+	and(...conds) {
+		return new LogicCondition("AND", conds);
+	},
+	or(...conds) {
+		return new LogicCondition("OR", conds);
+	},
+};
+
+console.log(
+	SQL.builder
+		.table("users")
+		.where(
+			SQL.and(
+				SQL.including("name", "john"),
+				SQL.gte("age", 20),
+				SQL.lte("age", 30)
+			)
+		)
+		.order("age", SQL.ASC) + ";"
+);
